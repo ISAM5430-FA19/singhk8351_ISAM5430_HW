2e784a86b4c0a0bd225c6b9781ec099bf0ff3891
singhkirti1101 <SinghK8351@Uhcl.edu>
2019-10-10 19:00:13 -0500

    Lecture changes

---
+++ HW05/B/Account.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace B
+{
+    public class Account
+    {
+        private string _name;
+        public float Withdraw(float amount, float bal)
+        {
+            if (amount > bal)
+            {
+                Console.WriteLine("Withdrawal amount exceeded account balance.");
+                return bal;
+            }
+            return bal - amount;
+        }
+        public Account(string name)
+        {
+            // D'Morgan's Law
+            // A||B == !(!A && !B)
+            // A&& B == !(!A || !B)
+            //if(!(name == null || name == ""))
+            // if(name != null && name != "")
+            if (!string.IsNullOrEmpty(name))
+                Name = name;
+            Is_Active = true;
+            IsActive = true;
+            //_name = name;
+
+            // Name = name;
+        }
+
+        public Account(string name, bool isActive)
+        {
+            Name = name;
+            IsActive = Is_Active = isActive;
+        }
+
+        public override string ToString()
+        {
+            //return GetType().FullName; // prints out the fully qualified type name
+            //return base.ToString();
+            if (IsActive)
+                return ($"Active Account {Name}");
+            else
+                return ($"Inactive Account {Name}");
+        }
+        public readonly bool Is_Active; // field
+
+        public bool IsActive
+        {
+            get; private set; // state
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value != null && value != "")
+                    _name = value;
+            }
+        }
+    }
+}
---
+++ HW05/B/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float balance = 1000;
+            // default constructor is parameterless
+            //Account account = new Account(); // instance of Account class i.e object
+            Account account = new Account("Kirti Singh", false); // parameterized constructor
+            //a constructor forces you initialize the required states
+            account.SetName("Kirti");
+            Console.WriteLine("The name1 is " + account.GetName());
+            //account.Name = "Wu";
+            account.Name = "";
+            //account.IsActive = true;
+            //account.Is_Active = true;
+            Console.WriteLine("Is the account active?" + account.IsActive);
+            Console.WriteLine("The name2 is " + account.Name);
+            // Fully qualified type name, which includes a namespace"dot"
+            Console.WriteLine(account);
+            Console.WriteLine("Enter the amount to be withdrawn: ");
+            float withdrawalAmt = float.Parse(Console.ReadLine());
+            balance = account.Withdraw(withdrawalAmt, balance);
+            Console.WriteLine("The account balance is: {0}", balance);
+        }
+    }
+}
+
